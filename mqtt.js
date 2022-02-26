9df4e3d71411c2a3c696e3bc1572e373ad10a6a6
John <john.olivierb@gmail.com>
2022-02-25 20:23:29 -0500

    Update MQTT var

--- mqtt.js
+++ mqtt.js
@@ -1,12 +1,14 @@
 module.exports = () => {
-    var mqtt = require('mqtt')
-    const host = '192.168.1.13'
-    const port = '1883'
-    var client = mqtt.connect(`mqtt://${host}:${port}`)
-    client.port = port;
-    client.host = host;
-    client.on('connect', function () {
-      console.log(`Connected to MQTT: mqtt://${client.host}:${client.port}`)
-    })
-    return client;
-  };
+	require('dotenv').config();
+	var mqtt = require('mqtt');
+	const {MQTT} = process.env;
+  const host = `${MQTT}`;
+	const port = '1883';
+	var client = mqtt.connect(`mqtt://${host}:${port}`);
+	client.port = port;
+	client.host = host;
+	client.on('connect', function () {
+		console.log(`Connected to MQTT: mqtt://${client.host}:${client.port}`);
+	});
+	return client;
+};
