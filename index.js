88b03b909728df9fb6e898bc8777a1de2ddfdeb8
John Barry <john.olivierb@gmail.com>
2022-04-04 20:49:48 -0400

    return

--- index.js
+++ index.js
@@ -125,7 +125,8 @@ eventEmitter.on('presence/true', async () => {
     await setState(7);
     await metrics({message: 'User Ready, waiting'});
 		while (!alluser) {
-			await checkUsers();
+      await checkUsers();
+      return
 		}
 		await scan();
 	}
