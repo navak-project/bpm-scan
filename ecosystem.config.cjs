1b736529f6583ef937c992a0668a14fc72f727e1
John Barry <john.olivierb@gmail.com>
2022-04-06 16:30:34 -0400

    add npadd dev and prod

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,16 +4,27 @@ module.exports = {
     script: "./index.js",
     env_production : {
         NODE_ENV : "production"
+    },
+    env_production : {
+      NODE_ENV: "dev"
     }
   }],
   deploy : {
+    dev : {
+       "user" : "pi",
+       "host" : ["192.168.1.224"],
+       "ref"  : "origin/dev",
+      "repo":   "https://github.com/navak-project/bpm-scan.git",
+       "path" : "../bpm-scan-dev",
+      "post-deploy": "git pull && npm i && pm2 startOrRestart ecosystem.congif.csj --env dev"
+    },
     production : {
        "user" : "pi",
        "host" : ["192.168.1.224"],
-       "ref"  : "origin/refactor",
+       "ref"  : "origin/master",
       "repo":   "https://github.com/navak-project/bpm-scan.git",
        "path" : "../bpm-scan-prod",
-       "post-deploy" : "git pull && pm2 resart 0"
+      "post-deploy": "git pull && npm i && pm2 startOrRestart ecosystem.congif.csj  --env production"
     }
   }
 }
