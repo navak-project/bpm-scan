130c8da2f977a83b3a38a78e4fb21ccab581fb0c
John Barry <john.olivierb@gmail.com>
2022-04-28 16:11:07 -0400

    test without event

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,17 +1,17 @@
 //import { eventEmitter } from '../index.js'
-const eventEmitter = require('../index.js')
+//const eventEmitter = require('../index.js')
 const Gpio = require('onoff').Gpio;
 const button = new Gpio(4, 'in', 'both');
 let presence = false;
 button.watch((err, value) => {
   console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
-  eventEmitter.emit('presence/test', value);
+ // eventEmitter.emit('presence/test', value);
 });
-export function getPresence() {
+/*export function getPresence() {
     button.watch((err, value) => {
       console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
-      eventEmitter.emit('presence/test', value);
+     // eventEmitter.emit('presence/test', value);
     });
   
   return presence
-}
+}*/
