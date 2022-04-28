1685b7f1cc01abeda071e6a0b2d4b681940f9b74
John Barry <john.olivierb@gmail.com>
2022-04-28 19:22:51 -0400

    remove a/d

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,24 +1,9 @@
 const { Gpio } = require('onoff');
-const mcpadc = require('mcp-spi-adc');
+
 let detection = false;
 let hasHand = false;
 // set BCM 17 pin as 'input'
 const switchIn = new Gpio('17', 'in', 'both');
-
-const ledOut = new Gpio('4', 'out');
-// listen for pin voltage change
-
-const tempSensor = mcpadc.open(5, { speedHz: 20000 }, err => {
-  if (err) throw err;
-
-  setInterval(_ => {
-    tempSensor.read((err, reading) => {
-      if (err) throw err;
-
-      console.log((reading.value * 3.3 - 0.5) * 100);
-    });
-  }, 1000);
-});
 
 switchIn.watch((err, value) => {
   console.log("🚀 ~ file: presence.cjs ~ line 12 ~ switchIn.watch ~ value", value);
