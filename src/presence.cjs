82cc0431de03e754b1d4b01049ffbd4f44d5b9ae
John Barry <john.olivierb@gmail.com>
2022-04-28 18:54:29 -0400

    test tempSensor

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,5 +1,5 @@
 const { Gpio } = require('onoff');
-
+const mcpadc = require('mcp-spi-adc');
 let detection = false;
 let hasHand = false;
 // set BCM 17 pin as 'input'
@@ -8,18 +8,28 @@ const switchIn = new Gpio('17', 'in', 'both');
 const ledOut = new Gpio('4', 'out');
 // listen for pin voltage change
 
+const tempSensor = mcpadc.open(5, { speedHz: 20000 }, err => {
+  if (err) throw err;
 
+  setInterval(_ => {
+    tempSensor.read((err, reading) => {
+      if (err) throw err;
+
+      console.log((reading.value * 3.3 - 0.5) * 100);
+    });
+  }, 1000);
+});
+
+switchIn.watch((err, value) => {
+  console.log("🚀 ~ file: presence.cjs ~ line 12 ~ switchIn.watch ~ value", value);
+  if (err) {
+    console.log('Error', err);
+  }
+  detection = true;
+  return
+});
 
 const blinkLed = _ => {
- switchIn.watch((err, value) => {
-   console.log("🚀 ~ file: presence.cjs ~ line 12 ~ switchIn.watch ~ value", value);
-   if (err) {
-     console.log('Error', err);
-   }
-   detection = true;
-   return
- });
-  console.log(switchIn.readSync() ^ 1);
   detection = false;
   //detection = false;
   setTimeout(blinkLed, 1200);
