8ceaf59294ce529756b1a41f6869fad8e6d8336b
John Barry <john.olivierb@gmail.com>
2022-04-28 17:23:50 -0400

    add light

--- src/presence.cjs
+++ src/presence.cjs
@@ -2,13 +2,13 @@ const { Gpio } = require('onoff');
 
 // set BCM 17 pin as 'input'
 const switchIn = new Gpio('17', 'in', 'both');
-
+const ledOut = new Gpio('4', 'out');
 // listen for pin voltage change
 switchIn.watch((err, value) => {
   if (err) {
     console.log('Error', err);
   }
 
-  // log pin value (0 or 1)
-  console.log('Pin value', value);
+  // write the input value (0 or 1) 'ledOut' pin
+  ledOut.writeSync(value);
 });
