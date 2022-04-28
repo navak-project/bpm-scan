19d60d9117be8454c090eba0408db04a3d963851
John Barry <john.olivierb@gmail.com>
2022-04-28 16:27:01 -0400

    .

--- src/presence.cjs
+++ src/presence.cjs
@@ -5,13 +5,17 @@ var gpiop = require('rpi-gpio').promise;
 const button = new Gpio(4, 'in', 'rising');
 console.log("🚀 ~ file: presence.cjs ~ line 5 ~ button", button);
 let presence = false;
-gpiop.setup(4, gpiop.DIR_OUT)
-.then(() => {
-    return gpiop.write(4, true)
-})
-.catch((err) => {
-    console.log('Error: ', err.toString())
-})
+
+function alert() {
+  console.log("detected !");
+}
+
+gpiop.on('change', function (channel, value) {
+  console.log("🚀 ~ file: presence.cjs ~ line 17 ~ value", value);
+  console.log("🚀 ~ file: presence.cjs ~ line 17 ~ channel", channel);
+  //send monitoring data to server for monitor on site
+});
+gpiop.setup(4, gpio.DIR_IN, gpio.EDGE_BOTH, alert);
 console.log("🚀 ~ file: presence.cjs ~ line 6 ~ presence", presence);
 button.watch((err, value) => {
   console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
