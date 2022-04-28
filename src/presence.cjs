950f1e59e176eba2115d84aab2666e0e0ebc36dc
John Barry <john.olivierb@gmail.com>
2022-04-28 17:54:46 -0400

    pin 7

--- src/presence.cjs
+++ src/presence.cjs
@@ -3,10 +3,13 @@ const { Gpio } = require('onoff');
 let detection = false;
 let hasHand = false;
 // set BCM 17 pin as 'input'
-const switchIn = new Gpio('17', 'in', 'both', { debounceTimeout: 10 });
+const switchIn = new Gpio('7', 'in', 'both', { debounceTimeout: 2 });
 
 const ledOut = new Gpio('4', 'out');
 // listen for pin voltage change
+setInterval(function () {
+  console.log(switchIn.readSync() ^ 1);
+}, 100);
 
 switchIn.watch((err, value) => {
   console.log("🚀 ~ file: presence.cjs ~ line 12 ~ switchIn.watch ~ value", value);
