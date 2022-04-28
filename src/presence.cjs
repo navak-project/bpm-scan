60dccdaf98fe009e51e645f049e0edb0133d10cd
John Barry <john.olivierb@gmail.com>
2022-04-28 16:14:26 -0400

    log

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,8 +1,10 @@
 //import { eventEmitter } from '../index.js'
 //const eventEmitter = require('../index.js')
 const Gpio = require('onoff').Gpio;
-const button = new Gpio(4, 'in', 'both');
+const button = new Gpio(4, 'in', 'rising', { debounceTimeout: 10 });
+console.log("🚀 ~ file: presence.cjs ~ line 5 ~ button", button);
 let presence = false;
+console.log("🚀 ~ file: presence.cjs ~ line 6 ~ presence", presence);
 button.watch((err, value) => {
   console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
  // eventEmitter.emit('presence/test', value);
