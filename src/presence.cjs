e88aa9b1aae11c1d9e22efb4683a06ded5abc34e
John Barry <john.olivierb@gmail.com>
2022-04-28 16:34:39 -0400

    onoff

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,30 +1,19 @@
 //import { eventEmitter } from '../index.js'
 //const eventEmitter = require('../index.js')
 const Gpio = require('onoff').Gpio;
-var gpiop = require('rpi-gpio').promise;
-//const button = new Gpio(4, 'in', 'rising');
+//var gpiop = require('rpi-gpio').promise;
+const button = new Gpio(4, 'in', 'both');
 let presence = false;
 
-function alert() {
-  console.log("detected !");
-}
 
-gpiop.on('change', function (channel, value) {
+/*gpiop.on('change', function (channel, value) {
   console.log("🚀 ~ file: presence.cjs ~ line 17 ~ value", value);
   console.log("🚀 ~ file: presence.cjs ~ line 17 ~ channel", channel);
   //send monitoring data to server for monitor on site
 });
-gpiop.setup(4, gpiop.DIR_IN, gpiop.EDGE_BOTH, alert);
-console.log("🚀 ~ file: presence.cjs ~ line 6 ~ presence", presence);
-/*button.watch((err, value) => {
+gpiop.setup(4, gpiop.DIR_IN, gpiop.EDGE_BOTH, alert);*/
+
+button.watch((err, value) => {
   console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
  // eventEmitter.emit('presence/test', value);
-});*/
-/*export function getPresence() {
-    button.watch((err, value) => {
-      console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
-     // eventEmitter.emit('presence/test', value);
-    });
-  
-  return presence
-}*/
+});
