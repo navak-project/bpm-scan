360d2e887f349ff665c6825ce67449905a776b15
John Barry <john.olivierb@gmail.com>
2022-04-28 16:09:57 -0400

    try cjs

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,9 +1,8 @@
-import { eventEmitter } from '../index.js'
-//const Gpio = require('onoff').Gpio;
-import Gpio from 'onoff';
+//import { eventEmitter } from '../index.js'
+const eventEmitter = require('../index.js')
+const Gpio = require('onoff').Gpio;
 const button = new Gpio(4, 'in', 'both');
 let presence = false;
-console.log("🚀 ~ file: presence.js ~ line 6 ~ presence", presence);
 button.watch((err, value) => {
   console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
   eventEmitter.emit('presence/test', value);
