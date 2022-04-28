a6ce3da214095726e3c6297063c57b3d15826b0c
John Barry <john.olivierb@gmail.com>
2022-04-28 18:09:22 -0400

    try in loop

--- src/presence.cjs
+++ src/presence.cjs
@@ -7,23 +7,20 @@ const switchIn = new Gpio('17', 'in', 'both');
 
 const ledOut = new Gpio('4', 'out');
 // listen for pin voltage change
-setInterval(function () {
-  console.log(switchIn.readSync() ^ 1);
-  detection = false;
-}, 100);
 
-switchIn.watch((err, value) => {
-  console.log("🚀 ~ file: presence.cjs ~ line 12 ~ switchIn.watch ~ value", value);
-  if (err) {
-    console.log('Error', err);
-  }
-  
-  detection = true;
- // return
-});
+
 
 const blinkLed = _ => {
- 
+ switchIn.watch((err, value) => {
+   console.log("🚀 ~ file: presence.cjs ~ line 12 ~ switchIn.watch ~ value", value);
+   if (err) {
+     console.log('Error', err);
+   }
+   detection = true;
+   return
+ });
+  console.log(switchIn.readSync() ^ 1);
+  detection = false;
   //detection = false;
   setTimeout(blinkLed, 1200);
   console.log("detection: " + detection);
