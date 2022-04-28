ca2e2f8ea448b5678d1d9c9e212f7a7471e71309
John Barry <john.olivierb@gmail.com>
2022-04-28 17:32:13 -0400

    debounce

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,9 +1,14 @@
 const { Gpio } = require('onoff');
 
 // set BCM 17 pin as 'input'
-const switchIn = new Gpio('17', 'in', 'both');
+const switchIn = new Gpio('17', 'in', 'rising', { debounceTimeout: 10 });
 const ledOut = new Gpio('4', 'out');
 // listen for pin voltage change
+switchIn.read()
+  .then((value) => {
+    console.log(value);
+  }).catch(err => console.log(err));
+
 switchIn.watch((err, value) => {
   console.log("🚀 ~ file: presence.cjs ~ line 8 ~ switchIn.watch ~ value", value);
   if (err) {
