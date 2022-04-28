46bc6dece2b93d8ef64fa835a9a39a26968d38bd
John Barry <john.olivierb@gmail.com>
2022-04-28 16:07:57 -0400

    import onoff

--- src/presence.js
+++ src/presence.js
@@ -1,7 +1,9 @@
 import { eventEmitter } from '../index.js'
-const Gpio = require('onoff').Gpio;
+//const Gpio = require('onoff').Gpio;
+import Gpio from 'onoff';
 const button = new Gpio(4, 'in', 'both');
 let presence = false;
+console.log("🚀 ~ file: presence.js ~ line 6 ~ presence", presence);
 button.watch((err, value) => {
   console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
   eventEmitter.emit('presence/test', value);
