324ba470507484a0125199d37afde7ecb6473cc9
John Barry <john.olivierb@gmail.com>
2022-04-28 17:37:01 -0400

    readSync

--- src/presence.cjs
+++ src/presence.cjs
@@ -3,13 +3,21 @@ const { Gpio } = require('onoff');
 let detection = false;
 // set BCM 17 pin as 'input'
 const switchIn = new Gpio('17', 'in', 'both', { debounceTimeout: 10 });
+console.log("🚀 ~ file: presence.cjs ~ line 6 ~ switchIn", switchIn);
 const ledOut = new Gpio('4', 'out');
 // listen for pin voltage change
-switchIn.read()
-  .then((value) => {
-    console.log(value);
-  }).catch(err => console.log(err));
-
+switchIn.readSync((err, value) => {
+  console.log("🚀 ~ file: presence.cjs ~ line 11 ~ switchIn", value);
+  if (err) {
+    console.log(err);
+  } else {
+    if (value === 1) {
+      detection = true;
+    } else {
+      detection = false;
+    }
+  }
+})
 switchIn.watch((err, value) => {
   detection = true;
   console.log("detection: " + detection);
