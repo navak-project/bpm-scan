db7ab032ed54b45d4a72d61ff82466056818f6bd
John Barry <john.olivierb@gmail.com>
2022-04-28 16:33:32 -0400

    remove onoff for testing

--- src/presence.cjs
+++ src/presence.cjs
@@ -2,7 +2,7 @@
 //const eventEmitter = require('../index.js')
 const Gpio = require('onoff').Gpio;
 var gpiop = require('rpi-gpio').promise;
-const button = new Gpio(4, 'in', 'rising');
+//const button = new Gpio(4, 'in', 'rising');
 console.log("🚀 ~ file: presence.cjs ~ line 5 ~ button", button);
 let presence = false;
 
@@ -17,10 +17,10 @@ gpiop.on('change', function (channel, value) {
 });
 gpiop.setup(4, gpiop.DIR_IN, gpiop.EDGE_BOTH, alert);
 console.log("🚀 ~ file: presence.cjs ~ line 6 ~ presence", presence);
-button.watch((err, value) => {
+/*button.watch((err, value) => {
   console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
  // eventEmitter.emit('presence/test', value);
-});
+});*/
 /*export function getPresence() {
     button.watch((err, value) => {
       console.log("🚀 ~ file: presence.js ~ line 5 ~ button.watch ~ value", value);
