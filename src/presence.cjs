451a7eeae5b7987a6ae4209527003c6a843d0313
John Barry <john.olivierb@gmail.com>
2022-04-28 17:14:52 -0400

    test pin in

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,13 +1,14 @@
 const { Gpio } = require('onoff');
 
 // set BCM 4 pin as 'output'
-const ledOut = new Gpio('4', 'out');
+const ledOut = new Gpio('4', 'in');
 
 // current LED state
 let isLedOn = false;
 
 // run a infinite interval
 setInterval(() => {
+console.log("🚀 ~ file: presence.cjs ~ line 5 ~ ledOut", ledOut);
   ledOut.writeSync(isLedOn ? 0 : 1); // provide 1 or 0 
   isLedOn = !isLedOn; // toggle state
 }, 3000); // 3s
