d74fb5ec70b7f64e43b750bfc56fa961b0c52012
John <john.olivierb@gmail.com>
2022-04-30 13:28:10 -0400

    remove presence

--- src/presence.cjs
+++ src/presence.cjs
@@ -1,23 +1,23 @@
-const { Gpio } = require('onoff');
+// const { Gpio } = require('onoff');
 
-let detection = false;
-let hasHand = false;
-// set BCM 17 pin as 'input'
-const switchIn = new Gpio('17', 'in', 'both');
+// let detection = false;
+// let hasHand = false;
+// // set BCM 17 pin as 'input'
+// const switchIn = new Gpio('17', 'in', 'both');
 
-switchIn.watch((err, value) => {
-  console.log("🚀 ~ file: presence.cjs ~ line 12 ~ switchIn.watch ~ value", value);
-  if (err) {
-    console.log('Error', err);
-  }
-  detection = true;
-  return
-});
+// switchIn.watch((err, value) => {
+//   console.log("🚀 ~ file: presence.cjs ~ line 12 ~ switchIn.watch ~ value", value);
+//   if (err) {
+//     console.log('Error', err);
+//   }
+//   detection = true;
+//   return
+// });
 
-const blinkLed = _ => {
-  detection = false;
-  //detection = false;
-  setTimeout(blinkLed, 1200);
-  console.log("detection: " + detection);
-};
-blinkLed();
+// const blinkLed = _ => {
+//   detection = false;
+//   //detection = false;
+//   setTimeout(blinkLed, 1200);
+//   console.log("detection: " + detection);
+// };
+// blinkLed();
