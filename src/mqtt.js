f5c730522caed48ad167fdbd83ba2a35ec104330
John Barry <john.olivierb@gmail.com>
2022-05-02 17:35:58 -0400

    fix await

--- src/mqtt.js
+++ src/mqtt.js
@@ -2,21 +2,17 @@ import 'dotenv/config';
 import mqtt from 'mqtt';
 const {MQTTIP, ID} = process.env;
 
-export async function clientConnect() {
+export function clientConnect() {
 	let client;
 	try {
     const host = MQTTIP;
     const port = '1883';
     client = mqtt.connect(`mqtt://${host}:${port}`);
-    client.on('connect', function () {
+    client.on('connect', async function () {
       console.log(`🚀 ~ Connected to MQTT broker: mqtt://${host}:${port}`);
       await client.subscribe(`/station/${ID}/presence`);
       await client.subscribe(`/station/${ID}/reboot`);
     });
-    client.on('message', function (topic, payload) {
-      console.log(topic)
-    });
-
 	} catch (err) {
 		console.log('🚀 ~ file: mqtt.js ~ line 39 ~ returnnewpromise ~ err', err);
 	}
