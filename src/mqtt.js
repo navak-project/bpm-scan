0fd2339011d0ef41da81e338c9fcf5ff77ef5a76
John Barry <john.olivierb@gmail.com>
2022-05-02 15:11:37 -0400

    set client

--- src/mqtt.js
+++ src/mqtt.js
@@ -3,22 +3,22 @@ import mqtt from 'mqtt';
 const {MQTTIP, ID} = process.env;
 
 export function clientConnect() {
-	let Bclient;
+	let client;
 	try {
     const host = MQTTIP;
     const port = '1883';
-    Bclient = mqtt.connect(`mqtt://${host}:${port}`);
-    Bclient.on('connect', function () {
+    client = mqtt.connect(`mqtt://${host}:${port}`);
+    client.on('connect', function () {
       console.log(`🚀 ~ Connected to MQTT broker: mqtt://${host}:${port}`);
-      Bclient.subscribe(`/station/${ID}/presence`);
-      Bclient.subscribe(`/station/${ID}/reboot`);
+      client.subscribe(`/station/${ID}/presence`);
+      client.subscribe(`/station/${ID}/reboot`);
     });
-    Bclient.on('message', function (topic, payload) {
+    client.on('message', function (topic, payload) {
       console.log(topic)
     });
 
 	} catch (err) {
 		console.log('🚀 ~ file: mqtt.js ~ line 39 ~ returnnewpromise ~ err', err);
 	}
-	return Bclient;
+  return client;
 }
