a37269aaacebaf5a398c8788da9d02be51b0ca8b
John Barry <john.olivierb@gmail.com>
2022-05-03 16:33:17 -0400

    try back ADS1x15 Interface over I2C

--- src/ads1x15.js
+++
@@ -1,7 +0,0 @@
-import { init } from 'raspi';
-import { I2C } from 'raspi-i2c';
-
-init(() => {
-  const i2c = new I2C();
-  console.log(i2c.readByteSync(0x18)); // Read one byte from the device at address 18
-});
