28eeb0d2ca5ae2590b849b59e01f3f74f414af0e
John Barry <john.olivierb@gmail.com>
2022-05-03 16:21:43 -0400

    try @abandonware/i2c

--- src/ads1x15.cjs
+++ src/ads1x15.cjs
@@ -1,37 +1,38 @@
-"use strict";
+var i2c = require('@abandonware/i2c');
+var address = 0x18;
+var wire = new i2c(address, { device: '/dev/i2c-1' }); // point to your i2c address, debug provides REPL interface
 
-const Raspi = require('raspi');
-const I2C = require('raspi-i2c').I2C;
-const ADS1x15 = require('raspi-kit-ads1x15');
-
-// Init Raspi
-Raspi.init(() => {
-
-  // Init Raspi-I2c
-  const i2c = new I2C();
-
-  // Init the ADC
-  const adc = new ADS1x15({
-    i2c,                                    // i2c interface
-    chip: ADS1x15.chips.IC_ADS1015,         // chip model
-    address: ADS1x15.address.ADDRESS_0x48,  // i2c address on the bus
-
-    // Defaults for future readings
-    pga: ADS1x15.pga.PGA_4_096V,            // power-gain-amplifier range
-    sps: ADS1x15.spsADS1015.SPS_250         // data rate (samples per second)
-  });
-
-  // Get a single-ended reading from channel-0 and display the results
-  adc.readChannel(ADS1x15.channel.CHANNEL_0, (err, value, volts) => {
-    if (err) {
-      console.error('Failed to fetch value from ADC', err);
-      process.exit(1);
-    } else {
-      console.log('Channel 0');
-      console.log(' * Value:', value);    // will be a 11 or 15 bit integer depending on chip
-      console.log(' * Volts:', volts);    // voltage reading factoring in the PGA
-      process.exit(0);
-    }
-  });
-
+wire.scan(function (err, data) {
+  console.log("🚀 ~ file: ads1x15.cjs ~ line 6 ~ data", data);
+  // result contains an array of addresses
 });
+
+wire.writeByte(byte, function (err) { });
+
+wire.writeBytes(command, [byte0, byte1], function (err) { });
+
+wire.readByte(function (err, res) { 
+console.log("🚀 ~ file: ads1x15.cjs ~ line 15 ~ res", res);
+  
+})
+
+  wire.readBytes(command, length, function (err, res) {
+    // result contains a buffer of bytes
+  });
+
+wire.on('data', function (data) {
+    console.log("🚀 ~ file: ads1x15.cjs ~ line 26 ~ data", data);
+
+    // result for continuous stream contains data buffer, address, length, timestamp
+  });
+
+  wire.stream(command, length, delay); // continuous stream, delay in ms
+
+
+  // plain read/write
+
+  wire.write([byte0, byte1], function (err) { });
+
+  wire.read(length, function (err, res) {
+    // result contains a buffer of bytes
+  });
