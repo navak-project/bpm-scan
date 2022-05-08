ced52fe591ccdd8ce6bc7d360921c9c9e9e0c603
John Barry <john.olivierb@gmail.com>
2022-05-08 16:52:13 -0400

    log data

--- src/artnet.cjs
+++ src/artnet.cjs
@@ -5,24 +5,6 @@ const ws281x = require('@gbkwiatt/node-rpi-ws281x-native');
 //import ws281x from '@gbkwiatt/node-rpi-ws281x-native';
 // Create new dmxnet instance
 
-/*var dmxnet = new dmxlib.dmxnet({
-	dma: 10,
-	freq: 800000,
-	gpio: 18,
-	invert: false,
-	brightness: 255,
-	stripType: ws281x.stripType.WS2812
-});
-
-var dmxnet2 = new dmxlib.dmxnet({
-	dma: 10,
-	freq: 800000,
-	gpio: 21,
-	invert: false,
-	brightness: 255,
-	stripType: ws281x.stripType.WS2812
-});*/
-
 var dmxnet = new dmxlib.dmxnet();
 //var dmxnet2 = new dmxlib.dmxnet();
 // Create a new receiver instance, listening for universe 5 on net 0 subnet 0
@@ -39,20 +21,6 @@ var receiver2 = dmxnet.newReceiver({
 });
 
 var receiver3 = dmxnet.newReceiver({
-  subnet: 15,
-  universe: process.env.UNI1,
-  net: 0
-});
-
-var receiver4 = dmxnet.newReceiver({
-  subnet: 15,
-  universe: process.env.UNI2,
-  net: 0
-});
-
-
-// Create a new receiver instance, listening for universe 5 on net 0 subnet 0
-/*var receiver3 = dmxnet.newReceiver({
 	subnet: 15,
 	universe: process.env.UNI1,
 	net: 0
@@ -63,11 +31,13 @@ var receiver4 = dmxnet.newReceiver({
 	universe: process.env.UNI2,
 	net: 0
 });
-*/
+
 // ---- trap the SIGINT and reset before exit
 process.on('SIGINT', function () {
-  console.log('Goodbye!');
-  process.nextTick(function () { process.exit(0); });
+	console.log('Goodbye!');
+	process.nextTick(function () {
+		process.exit(0);
+	});
 });
 
 /*const channels = ws281x.init({
@@ -91,50 +61,49 @@ const channel = ws281x(512, {
 const channel2 = ws281x(512, {
 	dma: 10,
 	freq: 800000,
-	gpio: 18,
+	gpio: 21,
 	invert: false,
 	brightness: 255,
 	stripType: ws281x.stripType.WS2812
 });
-//console.log("🚀 ~ file: artnet.cjs ~ line 83 ~ colors", colors);
-//const colors2 = channel2.array;
-  //await sleep(2000);
-  //channels[0].array[12] = 0xff0000;
-  receiver.on('data', function (data) {
-    for (let i = 0; i < data.length / 3; i++) {
-      channel.array[i] = rgb2Int(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
-    }
-  });
 
-  receiver2.on('data', function (data) {
-    for (let i = 0; i < data.length / 3; i++) {
-      channel.array[i + 170] = rgb2Int(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
-    }
-  });
+receiver.on('data', function (data) {
+	for (let i = 0; i < data.length / 3; i++) {
+		channel.array[i] = rgb2Int(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+	}
+});
 
-  receiver3.on('data', function (data) {
-    for (let i = 0; i < data.length / 3; i++) {
-      channel2.array[i + 170] = rgb2Int(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
-    }
-  });
+receiver2.on('data', function (data) {
+	for (let i = 0; i < data.length / 3; i++) {
+		channel.array[i + 170] = rgb2Int(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+	}
+});
 
-  receiver4.on('data', function (data) {
-    for (let i = 0; i < data.length / 3; i++) {
-      channel2.array[i + 170] = rgb2Int(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
-    }
-  });
+receiver3.on('data', function (data) {
+	for (let i = 0; i < data.length / 3; i++) {
+		channel2.array[i + 170] = rgb2Int(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+	}
+});
 
-  setInterval(function () {
-    ws281x.render();
-  }, 1000 / 60);
+receiver4.on('data', function (data) {
+  
+  console.log("🚀 ~ file: artnet.cjs ~ line 93 ~ data", data);
+
+	for (let i = 0; i < data.length / 3; i++) {
+		channel2.array[i + 170] = rgb2Int(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+	}
+});
+
+setInterval(function () {
+	ws281x.render();
+}, 1000 / 60);
 
 function rgb2Int(r, g, b) {
 	return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
 
-
 function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
 }
